953dfc1896cf4832c992c356eb0636927bb804a4
s1ddok <siddok@gmail.com>
2019-04-16 18:44:06 +0300

    introduce array extensions

---
+++ Sources/Array+Extensions.swift
@@ -0,0 +1,46 @@
+//
+//  Array+Extensions.swift
+//  Smelter
+//
+//  Created by Andrey Volodin on 16/04/2019.
+//
+
+extension Array {
+    public func reformatConvWeight(outputChannels: Int,
+                                   inputChannels: Int,
+                                   kernelHeight: Int,
+                                   kernelWidth: Int,
+                                   isTranspose: Bool) -> [Element] {
+        var data: [Element] = [Element](repeating: self[0], count: self.count)
+        for oc in 0..<outputChannels {
+            for ic in 0..<inputChannels {
+                for kh in 0..<kernelHeight {
+                    for kw in 0..<kernelWidth {
+                        let inputIdx: Int
+                        let outputIdx: Int
+
+                        if isTranspose {
+                            inputIdx = ic * outputChannels * kernelHeight * kernelWidth
+                                     + oc * kernelHeight * kernelWidth
+                                     + kh * kernelWidth + kw
+                            outputIdx = oc * kernelHeight * kernelWidth * inputChannels
+                                     + (kernelHeight - 1 - kh) * kernelWidth * inputChannels
+                                     + (kernelWidth - 1 - kw) * inputChannels + ic
+                        } else {
+                            inputIdx = oc * inputChannels * kernelHeight * kernelWidth
+                                     + ic * kernelHeight * kernelWidth
+                                     + kh * kernelWidth + kw
+                            outputIdx = oc * inputChannels * kernelHeight * kernelWidth
+                                      + kh * kernelWidth * inputChannels
+                                      + kw * inputChannels + ic
+                        }
+
+                        data[outputIdx] = self[inputIdx]
+                    }
+                }
+            }
+        }
+
+        return data
+    }
+}
