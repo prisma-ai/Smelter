aab4ef9631981e745d3835cdfef4d766ef2300cf
Eugene Bokhan <eugenebokhan@protonmail.com>
2020-10-26 18:48:14 +0300

    update package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Smelter",
     platforms: [
-        .iOS(SupportedPlatform.IOSVersion.v11),
+        .iOS(.v11),
         .macOS(.v10_13)
     ],
     products: [
@@ -14,25 +14,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/s1ddok/Alloy.git",
-                 .branch("swiftpm2")),
+                 .branch("swiftpm3")),
         .package(name: "SwiftProtobuf",
                  url: "https://github.com/apple/swift-protobuf.git",
                  .exact("1.7.0"))
     ],
     targets: [
         .target(name: "Smelter",
-                dependencies: ["Alloy", "SwiftProtobuf"],
-                path: "Sources",
-                exclude: [],
-                sources: nil,
-                publicHeadersPath: nil,
-                cSettings: nil,
-                cxxSettings: nil,
-                swiftSettings: nil,
-                linkerSettings: [
-                    .linkedFramework("Metal"),
-                    .linkedFramework("MetalPerformanceShaders"),
-                    .linkedFramework("CoreGraphics")
-                ])
+                dependencies: ["Alloy", "SwiftProtobuf"])
     ]
 )
