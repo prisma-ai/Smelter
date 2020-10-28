65133745e50146a8d4ba6f786ab1e9f4e21a39fd
eugenebokhan <eugenebokhan@protonmail.com>
2020-10-28 06:16:57 +0300

    Introduce SwiftPM Support (#19)

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "Smelter",
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v10_13)
+    ],
+    products: [
+        .library(name: "Smelter",
+                 targets: ["Smelter"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/s1ddok/Alloy.git",
+                 .upToNextMajor(from:"0.16.3")),
+        .package(name: "SwiftProtobuf",
+                 url: "https://github.com/apple/swift-protobuf.git",
+                 .exact("1.7.0"))
+    ],
+    targets: [
+        .target(name: "Smelter",
+                dependencies: ["Alloy", "SwiftProtobuf"])
+    ]
+)
